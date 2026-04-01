44bed35520b719d3daccf3310dd3ce6b2d10c673
Maksim Klimov <maxklimov13@yandex.ru>
2026-04-01 19:33:53 +0200

    remove playground

--- playground/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { playableConfig } from "../src";
-import { NetworkName } from "../src/networkConfigs";
-
-export default ({ mode }: { mode: string }) =>
-  playableConfig({
-    network: mode as NetworkName,
-  });
