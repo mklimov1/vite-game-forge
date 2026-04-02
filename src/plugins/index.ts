caa8c23b60626ca6cb3a86216ee0f2bd065c88da
Maksim Klimov <maxklimov13@yandex.ru>
2026-04-02 19:26:33 +0200

    fix: remove assetpack from plugins index.ts

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -2,4 +2,3 @@ export { ctaPlugin } from "./cta";
 export { scriptsPlugin } from "./scriptsPlugin";
 export { sizeReportPlugin } from "./sizeReport";
 export { inlineAssetsPlugin } from "./inlineAssets/inlineAssetsPlugin";
-export { assetpackPlugin } from "./assetpack/assetpackPlugin";
